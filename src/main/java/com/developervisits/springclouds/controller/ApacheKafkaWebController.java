39ba893be7636b6d8af784789bddccb5b81027b0
Praveen Verma <praveen.verma@hcl.com>
2020-02-13 15:59:43 +0530

    initial commit

---
+++ src/main/java/com/developervisits/springclouds/controller/ApacheKafkaWebController.java
@@ -0,0 +1,25 @@
+package com.developervisits.springclouds.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.developervisits.springclouds.services.KafkaSender;
+
+@RestController
+@RequestMapping(value = "/developervisits-kafka/")
+public class ApacheKafkaWebController {
+
+	@Autowired
+	KafkaSender kafkaSender;
+
+	@GetMapping(value = "/producer")
+	public String producer(@RequestParam("message") String message) {
+		kafkaSender.send(message);
+
+		return "Message sent to the Kafka Successfully";
+	}
+
+}
---
+++ src/test/java/com/developervisits/springclouds/SpringKafkaHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developervisits.springclouds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
